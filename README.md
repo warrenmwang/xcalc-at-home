2a5518073267089940c645bb39d4591586ed79ff
Warren Wang <warren.wang2020@gmail.com>
2024-10-17 12:37:05 -0700

    get some square shapes on the screen.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ system on a common Linux OS like Ubuntu, my daily driver.
 Resources
 - https://www.youtube.com/watch?v=d2E7ryHCK08
 - https://stackoverflow.com/questions/10792361/how-do-i-gracefully-exit-an-x11-event-loop
+- https://tronche.com/gui/x/xlib
--- main.c
+++ main.c
@@ -1,14 +1,38 @@
+#include <X11/X.h>
 #include <X11/Xlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+void drawThings(Display* display, Window window, unsigned int screenNumber)
+{
+    GC graphicsContext = DefaultGC(display, screenNumber);
+    char str[] = "Hello X.org!";
+    int strLen = strlen(str);
+    XDrawString(display, window, graphicsContext, 100, 100, str, strLen);
+
+    int SCALE = 100;
+
+    // Draw a diamond
+    XPoint points[] = {
+       {1*SCALE, 2*SCALE},
+       {2*SCALE, 1*SCALE},
+       {3*SCALE, 2*SCALE},
+       {2*SCALE, 3*SCALE},
+    };
+    XDrawPoints(display, window, graphicsContext, points, 4, CoordModeOrigin);
+    XDrawLines(display, window, graphicsContext, points, 4, CoordModeOrigin);
+    XDrawLine(display, window, graphicsContext, points[0].x, points[0].y, points[3].x, points[3].y);
+
+    XDrawRectangle(display, window, graphicsContext, 150, 150, 30, 30);
+}
 
 int main()
 {
     Display* display = XOpenDisplay(NULL);
     int x = 50;
     int y = 50;
-    unsigned int width = 250;
-    unsigned int height = 250;
+    unsigned int width = 500;
+    unsigned int height = 500;
     unsigned int borderWidth = 1;
     unsigned int screenNumber = 0;
 
@@ -18,9 +42,6 @@ int main()
 
     XMapWindow(display, window); // map window to screen (shows the created window)
     XSelectInput(display, window, ExposureMask);
-
-    char str[] = "Hello X.org!";
-    int strLen = strlen(str);
 
     Atom wmDeleteMessage = XInternAtom(display, "WM_DELETE_WINDOW", False);
     XSetWMProtocols(display, window, &wmDeleteMessage, 1);
@@ -32,7 +53,7 @@ int main()
 
         switch(event.type) {
             case Expose:
-                XDrawString(display, window, DefaultGC(display, screenNumber), 100, 100, str, strLen);
+                drawThings(display, window, screenNumber);
                 break;
             case ClientMessage:
                 if (event.xclient.data.l[0] == wmDeleteMessage) {
